ba40936797307a8952d28bff8fa471f4574e9da4
Nick Noble <nickynoble@me.com>
2014-08-08 13:46:06 -0400

    fix bad variable

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 				
 					<h2><a href="<?php the_permalink() ?>"><?php the_title();?></a></h2>
 
-					<?php if (has_post_thumbnail( $post_id )) : // If the post has a thumbnail, show it: ?>
+					<?php if (has_post_thumbnail()) : // If the post has a thumbnail, show it: ?>
 						<a href="<?php the_permalink();?>"><?php the_post_thumbnail();?></a>
 					<?php endif; ?>
 
--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 	
 			<h2><?php the_title();?></h2>
 
-			<?php if (has_post_thumbnail( $post_id )) : // If the post has a thumbnail, show it: ?>
+			<?php if (has_post_thumbnail()) : // If the post has a thumbnail, show it: ?>
 				<a href="<?php the_permalink();?>"><?php the_post_thumbnail();?></a>
 			<?php endif; ?>
 
