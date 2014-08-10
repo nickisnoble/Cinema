f6114cd13cb085836eda21f83866ded7793ae65c
Nick Noble <nickynoble@me.com>
2014-08-10 16:32:30 -0400

    add post classes and IDs

--- page.php
+++ page.php
@@ -1,19 +1,18 @@
-		<?php get_header(); // Load header.php ?>
+<?php get_header(); // Load header.php ?>
+		
+	<?php if (have_posts()) : // Check for posts ?>
+		<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+				<div class="container">
 
-		<article>
-			<div class="container">
-				
-				<?php if (have_posts()) : // Check for posts ?>
-					<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
+					<h2><?php the_title();?></h2>
+					
+					<?php the_content(); // Get the post body ?>
 
-						<h2><?php the_title();?></h2>
-						
-						<?php the_content(); // Get the post body ?>
-				
-					<?php endwhile; ?>
-				<?php endif; ?>
-				
-			</div>
-		</article>
+				</div>
+			</article>
+	
+		<?php endwhile; ?>
+	<?php endif; ?>
 
-		<?php get_footer(); // Load footer.php ?>
+<?php get_footer(); // Load footer.php ?>
--- single.php
+++ single.php
@@ -1,23 +1,22 @@
 <?php get_header(); // Load header.php ?>
+
+	<?php if (have_posts()) : // Check for posts ?>
+		<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+				<div class="container">
 		
-<article>
-	<div class="container">
+					<h2><?php the_title();?></h2>
 
-		<?php if (have_posts()) : // Check for posts ?>
-			<?php while (have_posts()) : the_post(); // If there are posts, do each like this: ?>
+					<?php if (has_post_thumbnail()) : // If the post has a thumbnail, show it: ?>
+						<a href="<?php the_permalink();?>"><?php the_post_thumbnail();?></a>
+					<?php endif; ?>
+
+					<?php the_content(); // Get the post body ?>
+				</div>
+			</article>
 		
-				<h2><?php the_title();?></h2>
-
-				<?php if (has_post_thumbnail()) : // If the post has a thumbnail, show it: ?>
-					<a href="<?php the_permalink();?>"><?php the_post_thumbnail();?></a>
-				<?php endif; ?>
-
-				<?php the_content(); // Get the post body ?>
-		
-			<?php endwhile; ?>
-		<?php endif; ?>
-
-	</div>
-</article>
+		<?php endwhile; ?>
+	<?php endif; ?>
 
 <?php get_footer(); // Load footer.php ?>
