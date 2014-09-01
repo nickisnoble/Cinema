859694975c59d75c9b5f4c37246bb7acfc46483e
Nick Noble <nickynoble@me.com>
2014-09-01 15:09:40 -0400

    tweak js

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@
     e.preventDefault;
     $('body').toggleClass('menu-open');
   });
-  $('body.menu-open').click( function(event){ // if the menu is open and they click
+  $('.menu-open').click( function(event){ // if the menu is open and they click
      // Find out what they clicked on.
       var target = $( event.target );
       // If they click on a nav link...
       if(target.is('.site-navigation a')){
         // Do nothing.
       }else{
-        // Otherwise, close the menu
+        event.preventDefault;
         $('body').toggleClass('menu-open');
       }
   });
