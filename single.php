0cb6086a1ca12d9f1753b937afb54a9619b9af4c
Nick Noble <nickynoble@me.com>
2014-09-01 15:55:54 -0400

    add wrapper for author info

--- single.php
+++ single.php
@@ -9,21 +9,26 @@
 					<h1><?php the_title();?></h1>
 
 					<?php if (has_post_thumbnail()) : // If the post has a thumbnail, show it: ?>
-						<br>
+						<br><!-- correct spacing for image -->
 						<a href="<?php the_permalink();?>"><?php the_post_thumbnail();?></a>
 					<?php endif; ?>
-
 					
-					<div class="two-thirds">
+					<div class="two-thirds pull-right">
 						<?php the_content(); // Get the post body ?>
 					</div>
 
-					<div class="one-third pull-right">
-						<?php // Get Author info ?>
-						<?php echo get_avatar( get_the_author_meta( 'ID' ), 64 ); ?>
-						<h3><?php the_author(); ?></h3>
-						<p><?php get_the_author_meta('description'); ?></p>
-					</div>
+					<aside class="one-third">
+
+						<div class="author-box">
+							<?php // Get Author info ?>
+							<div class="author-photo">
+								<?php echo get_avatar( get_the_author_meta( 'ID' ), 64 ); ?>
+							</div>
+							<p class="lead author-name"><?php the_author(); ?></p>
+							<p class="author-bio"><?php get_the_author_meta('description'); ?></p>
+						</div>
+
+					</aside>
 
 				</div>
 			</article>
