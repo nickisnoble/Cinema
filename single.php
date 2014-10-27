1b9ce46da9a1582d836ac7dfd25066d9bbe91e1a
Nick Noble <nickynoble@me.com>
2014-10-27 01:12:16 -0400

    make author box only appear if there is a bio, and headshot only appear with valid gravatar

--- single.php
+++ single.php
@@ -13,22 +13,38 @@
 						<?php the_post_thumbnail();?>
 					<?php endif; ?>
 					
-					<div class="two-thirds">
-						<?php the_content(); // Get the post body ?>
-					</div>
+					<?php the_content(); // Get the post body ?>
 
-					<aside class="one-third">
+					<?php 
+					  global $user_ID;
 
-						<div class="author-box">
-							<?php // Get Author info ?>
-							<div class="author-photo">
-								<?php echo get_avatar( get_the_author_meta( 'ID' ), 64 ); ?>
-							</div>
+					  function validate_gravatar($email) {
+							// Craft a potential url and test its headers
+							$hash = md5(strtolower(trim($email)));
+							$uri = 'http://www.gravatar.com/avatar/' . $hash . '?d=404';
+							$headers = @get_headers($uri);
+							if (!preg_match("|200|", $headers[0])) {
+								$has_valid_avatar = FALSE;
+							} else {
+								$has_valid_avatar = TRUE;
+							}
+							return $has_valid_avatar;
+						}
+
+					  if ( get_the_author_meta('description',$user_ID) ) : // If a user has filled out their decscription show a bio on their entries 
+					?>
+
+					  <aside class="author-box">
+					  	<?php if ( validate_gravatar( get_the_author_meta('user_email',$user_ID) ) ) : ?>
+								<div class="author-photo">
+									<?php echo get_avatar( get_the_author_meta( 'ID' ), 64 ); ?>
+								</div>
+							<?php endif; ?>
 							<p class="lead author-name">About <?php the_author(); ?></p>
 							<p class="author-bio"><?php echo get_the_author_meta('description'); ?></p>
-						</div>
+						</aside>
 
-					</aside>
+					<?php endif; ?>
 
 				</div>
 			</article>
@@ -37,3 +53,11 @@
 	<?php endif; ?>
 
 <?php get_footer(); // Load footer.php ?>
+
+
+<?php 
+  global $user_ID;
+  if ( get_the_author_meta('description',$user_ID) ) : // If a user has filled out their decscription show a bio on their entries 
+?>
+  
+<?php endif; ?>
